1709d0b4cd86ff34caabe154646d07a9611b93f5
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 01:23:59 +0700

    Add files via upload

---
+++ cell01/ex01/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+say = input('What you gotta say?: ')
+while True :
+    got = input('I got that! Anything else? :')
+    if got=='stop...':
+        print('I got that! Anything else? : STOP')
+        break
+    
