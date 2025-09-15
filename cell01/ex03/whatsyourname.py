ecb63983026a19cc33a2eeb2dc21030544f8b05e
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 00:32:12 +0700

    Update whatsyourname.py

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,6 +1,7 @@
-first_name = 'Tharathep '
-last_name = 'Jucharoen'
+first_name = input("Enter first name = ")
+last_name = input("Enter last name = ")
 whole_name = first_name + last_name
 print(f"Hey what's your first name? : {first_name}")
 print(f"And your last name? : {last_name}")
+
 print(f"Well,pleased to meet you, {whole_name}")
