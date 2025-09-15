0307d03a021aafa08da6f5e28c847e46e2e2906d
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 01:26:50 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,5 @@
+num = int(input('Enter the number less than 25: '))
+for i in range(num,26):
+    print(f"Inside the loop, my variable is {i}")
+if num >25:
+    print('Error')
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,3 @@
+num = int(input('Enter number '))
+for i in range(0,num+1):
+    print(f"{i} x {num} = {i*num}")
