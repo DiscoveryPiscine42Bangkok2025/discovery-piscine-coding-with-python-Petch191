089d77b69cd5a6985a00ade54094e2d04a3c82f3
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 02:12:54 +0700

    Update to25.py

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,5 +1,6 @@
 num = int(input('Enter the number less than 25: '))
-for i in range(num,26):
-    print(f"Inside the loop, my variable is {i}")
-if num >25:
-    print('Error')
+while(num<25):
+    num+=1
+    print(f"Inside the loop, my variable is {num}")
+if num>25:
+    print("Error")
