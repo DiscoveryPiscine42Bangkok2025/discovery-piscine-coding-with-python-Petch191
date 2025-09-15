4fce6243cf188d1df36d16325a91ac64304bb20e
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 01:23:26 +0700

    Delete cell01/ex01/ex02 directory

--- cell01/ex01/ex02/i_got_that.py
+++
@@ -1,7 +0,0 @@
-say = input('What you gotta say?: ')
-while True :
-    got = input('I got that! Anything else? :')
-    if got=='stop...':
-        print('I got that! Anything else? : STOP')
-        break
-    
