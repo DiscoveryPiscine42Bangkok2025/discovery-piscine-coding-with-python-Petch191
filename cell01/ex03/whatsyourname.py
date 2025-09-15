e489396df5c36b8363b14eb303161b9b7a854835
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-15 15:40:37 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+first_name = 'Tharathep '
+last_name = 'Jucharoen'
+whole_name = first_name + last_name
+print(f"Hey what's your first name? : {first_name}")
+print(f"And your last name? : {last_name}")
+print(f"Well,pleased to meet you, {whole_name}")
