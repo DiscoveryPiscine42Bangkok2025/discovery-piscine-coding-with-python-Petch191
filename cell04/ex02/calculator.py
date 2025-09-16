9ef526aac67efd78b0c42c16c170e4675efbbafa
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 17:45:30 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,14 +1,22 @@
-def add(x,y):
-    return x+y
-def sub(x,y):
-    return x-y
-def divide(x,y):
-    return x/y
-def multi(x,y):
-    return x*y
-first_number = int(input('Give me the first number: '))
-second_number = int(input('Give me the second number: '))
-print(str(first_number)+" + "+str(second_number)+" = "+str(add(first_number,second_number)))
-print(str(first_number)+" - "+str(second_number)+" = "+str(sub(first_number,second_number)))
-print(str(first_number)+" / "+str(second_number)+" = "+str(divide(first_number,second_number)))
-print(str(first_number)+" x "+str(second_number)+" = "+str(multi(first_number,second_number)))
+try:
+    def add(x,y):
+        return x+y
+    def sub(x,y):
+        return x-y
+    def divide(x,y):
+        return x/y
+    def multi(x,y):
+        return x*y
+    first_number = int(input('Give me the first number: '))
+    second_number = int(input('Give me the second number: '))
+    print(str(first_number)+" + "+str(second_number)+" = "+str(add(first_number,second_number)))
+    print(str(first_number)+" - "+str(second_number)+" = "+str(sub(first_number,second_number)))
+    print(str(first_number)+" x "+str(second_number)+" = "+str(multi(first_number,second_number)))
+    try:
+        print(str(first_number)+" / "+str(second_number)+" = "+str(divide(first_number,second_number)))
+    except:
+        print('Sorry ! You are dividing by zero ')
+except:
+    print("Error")
+
+
