3068227ef4c5cd6257e95a39f10c860a2f0d0a70
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 19:33:44 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,6 +1,11 @@
-def age(your_age):
-    print("You are currently " + str(your_age) + " years old.")
-u_age = int(input("Please tell me your age: "))
-age("in 10 years,you'll be "+str(u_age+10)+" year old.")
-age("in 20 years,you'll be "+str(u_age+20)+" year old.")
-age("in 30 years,you'll be "+str(u_age+30)+" year old.")
+your_age = int(input("Please tell me your age: "))
+print("You are currently " + str(your_age) + " years old.")
+print("in 10 years,you'll be "+str(your_age+10)+" year old.")
+print("in 20 years,you'll be "+str(your_age+20)+" year old.")
+print("in 30 years,you'll be "+str(your_age+30)+" year old.")
+
+
+
+
+
+
