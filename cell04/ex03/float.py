875150355f16d6d2b9e4d55901056a61f0012a96
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 19:34:40 +0700

    Update float.py

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,7 +1,6 @@
-def f():
-    your_float = float(input('Give me a number: '))
-    if your_float == int(your_float):
-        print("This number a integer.")
-    else:
-        print("This number an decimal.")
-f()
+your_float = float(input('Give me a number: '))
+if your_float == int(your_float):
+    print("This number a integer.")
+else:
+    print("This number an decimal.")
+
