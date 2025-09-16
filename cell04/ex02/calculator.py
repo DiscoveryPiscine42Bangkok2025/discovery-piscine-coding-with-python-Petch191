63b5947106bcd7c24f63b981912fe494b74ea3b9
thjuchar(Petch) <66050191@kmitl.ac.th>
2025-09-16 17:53:58 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -7,16 +7,14 @@ try:
         return x/y
     def multi(x,y):
         return x*y
-    first_number = int(input('Give me the first number: '))
-    second_number = int(input('Give me the second number: '))
+    first_number =  int(input("Give me the first number = "))
+    second_number = int(input("Give me the second number = "))
     print(str(first_number)+" + "+str(second_number)+" = "+str(add(first_number,second_number)))
     print(str(first_number)+" - "+str(second_number)+" = "+str(sub(first_number,second_number)))
-    print(str(first_number)+" x "+str(second_number)+" = "+str(multi(first_number,second_number)))
     try:
         print(str(first_number)+" / "+str(second_number)+" = "+str(divide(first_number,second_number)))
     except:
-        print('Sorry ! You are dividing by zero ')
+         print('Sorry ! You are dividing by zero ')
+    print(str(first_number)+" x "+str(second_number)+" = "+str(multi(first_number,second_number)))
 except:
     print("Error")
-
-
